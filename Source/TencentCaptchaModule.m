8921f50198c232d6622bae7c369a0789ccf975aa
kang <kang@foxmail.com>
2018-09-07 19:18:25 +0800

    fix bug

--- Source/TencentCaptchaModule.m
+++ Source/TencentCaptchaModule.m
@@ -1,9 +1,3 @@
-//
-//  TencentCaptchaModule.m
-//  ErosPluginUMAnalytics
-//
-//  Created by XHY on 2018/7/18.
-//
 
 #import "TencentCaptchaModule.h"
 #import <WeexPluginLoader/WeexPluginLoader/WeexPluginLoader.h>
@@ -16,45 +10,15 @@ WX_PlUGIN_EXPORT_MODULE(tencentCaptcha, TencentCaptchaModule)
 
 @synthesize weexInstance;
 
-WX_EXPORT_METHOD(@selector(showCaptcha:))
+WX_EXPORT_METHOD(@selector(showCaptcha:successCallback:))
 
-/** 初始化友盟方法 */
+/** 加载腾讯验证码 */
 - (void)showCaptcha:(NSString *)appkey successCallback:(WXModuleCallback)successCallback
 {
-    /* 初始化友盟组件 */
-    // [UMConfigure initWithAppkey:appkey channel:nil];
-
-    /**
-	@param appid，在验证码接入平台注册申请；
-	@param callback，为回调函数，验证码验证完成后回调该函数将结果带回
-	*/
-	[[TCWebCodesBridge sharedBridge]loadTencentCaptcha:self.view appid:appkey callback:^(NSDictionary *resultJSON) {
-	    if(0==[resultJSON[@"ret"] intValue]) {
-	        /**
-	        验证成功
-	        返回内容：
-	        resultJSON[@"appid"]为回传的业务appid
-	        resultJSON[@"ticket"]为验证码票据
-	        resultJSON[@"randstr"]为随机串
-	        */
-	    } else {
-	        /**
-	        验证失败
-	        返回内容：
-	        ret=-1001为验证码js加载错误
-	        ret=-1002一般为网络错误
-	        ret=-1为返回票据数据解析错误，业务可根据需要重试处理
-	        ret的其他返回值，为验证失败，比如用户主动关闭了验证码弹框
-	        */
-	    }
-
-	    /* 成功回调 */
-        if (successCallback) {
-            successCallback(resultJSON);
-        }
-
-	}];
-
+    // 加载腾讯验证码
+    [[TCWebCodesBridge sharedBridge] loadTencentCaptcha:self.weexInstance.viewController.view appid:appkey callback:^(NSDictionary *resultJSON) { // appid在验证码接入平台注册申请，此处的1234为测试用appid
+        successCallback(resultJSON);
+    }];
+    
 }
-
 @end
